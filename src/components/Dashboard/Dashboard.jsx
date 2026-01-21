84e02f7d45e14b1afbfce10890e09c93328cb52c
Ami <ami.dorsey@me.com>
2026-01-21 15:59:01 -0500

    Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,33 +1,85 @@
 import { useEffect, useContext, useState } from 'react';
-
+import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 
-import * as userService from '../../services/userService';
+import * as userService from '../../services/letterService';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
-  const [users, setUsers] = useState([]);
+  const [letters, setLetters] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchLetters = async () => {
       try {
-        const fetchedUsers = await userService.index();
-        setUsers(fetchedUsers);
+        const fetchedLetters = await letterService.index();
+        setLetters(fetchedLetters);
       } catch (err) {
         console.log(err)
       }
     }
-    if (user) fetchUsers();
+    if (user) fetchLetters();
   }, [user]);
+
+  const handleDelete = async (letterId) => {
+    try {
+      await letterService.deletLetter(letterId);
+      setLetters(letters.filter((letter) => letter._id !== letterId));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // Filter letters
+  const waitingLetters = letters.filter((letter) => !letter.isDelivered);
+  const openedLetters = letters.filter((letter) => letter.isDelivered);
 
   return (
     <main>
-      <h1>Welcome, {user.username}</h1>
-       {users.map((item) => (
-        <p key={item._id}> {item.username}</p>
-      ))}
+      <h1>SoulMail: Whispers to Myself</h1>
+      <p>Leave yourself a whisper</p>
+
+      <h2>Elevated Salutaions, {user.username}</h2>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/' onClick={() => {
+          localStorage.removeItem('token');
+          window.location.reload();
+        }}>Sign Out</Link>
+      </nav>
+
+      <link to='/letters/new'>Sire your new whisper</link>
+      <section>
+        <h3>Waiting to be Opened ({waitingLetters.length})</h3>
+        {waitingLetters.length === 0 ? (
+          <p>No Letters</p>
+        ) : (
+          waitingLetters.map((letter) => (
+            <div key = {letter.id}>
+              <span>{letter.tilte}</span>
+              <span> Delivery Date: {new Date(letter.deliverAt).toLocaleDateString()}</span>
+              <Link to={`/letters/${letter._id}/edit`}>Edit Date</Link>
+              <button onClick={() => handleDelete(letter._id)}>Delete</button>
+            </div>
+          ))
+        )}
+      </section>
+
+      <section>
+        <h3>Opened ({openedLetters.length})</h3>
+        {openedLetters.length === 0 ? (
+          <p> No opened letters yet.</p>
+        ) : (
+          openedLetters.map((letter) => (
+           <div key={letter._id}>
+              <span>{letter.tilte}</span>
+              <span> Delivered: {new Date(letter.deliverAt).toLocaleDateString()}</span>
+              <Link to={`/letters/${letter._id}/edit`}>View</Link>
+              <button onClick={() => handleDelete(letter._id)}>Delete</button>
+           </div> 
+          ))
+        )}
+      </section>
     </main>
-  )
+  );
 };
 
 export default Dashboard;
